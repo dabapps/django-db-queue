89689c72d0ced0c6d4b5f0f10017491e6564e8d3
Jamie Matthews <jamie.matthews@gmail.com>
2015-05-14 14:48:28 +0100

    Wrap the deletion functionality in a management command

---
+++ django_dbq/management/commands/delete_old_jobs.py
@@ -0,0 +1,11 @@
+from django.core.management.base import BaseCommand
+from django_dbq.models import Job
+
+
+class Command(BaseCommand):
+
+    help = "Delete old jobs"
+
+    def handle(self, *args, **options):
+        Job.objects.delete_old()
+        self.stdout.write('Deleted old jobs')
