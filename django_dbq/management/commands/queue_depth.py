8cd9516225665b6942602e772f3cf4bdf70ddbe9
Jordan Brown <hello@jordaneb.com>
2019-11-28 15:58:32 +0000

    Add queue_depth management command

---
+++ django_dbq/management/commands/queue_depth.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from django_dbq.models import Job
+
+
+class Command(BaseCommand):
+
+    help = "Print the current depth of the given queue"
+
+    def add_arguments(self, parser):
+        parser.add_argument('queue_name', nargs='?', default='default', type=str)
+
+    def handle(self, *args, **options):
+        queue_name = options['queue_name']
+        queue_depths = Job.get_queue_depths_dict()
+
+        self.stdout.write('queue_name={queue_name} queue_depth={depth}'.format(queue_name=queue_name, depth=queue_depths.get(queue_name, 0)))
